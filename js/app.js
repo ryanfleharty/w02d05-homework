64b26ca1e5345d69ce63e39ee19d679286dc9135
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-03-30 09:30:37 -0600

    html and css on mvp done.

---
+++ js/app.js
@@ -0,0 +1,46 @@
+
+//make calculator in html
+//style it in css
+//create screen obj
+ const add = 
+
+ }
+ const sub = null
+ const div = null
+ const mult = null
+const screen = {
+    input: [],
+    output: [],
+}
+//create button class
+class Button {
+    constructor(value,target){
+        this.value = value;
+        this.target = target;
+    }
+}
+//render() function that renders the calculator at load and resets (c button)
+const render = () => {
+    !screen.input;
+    !screen.output; 
+}
+    //it should be called when the C button is 
+//input() function that takes in the click and adds to input array
+const input = () => {
+    if (this.target) {
+        console.log(screen.input.push(this.input))
+    }
+}
+    //look for and test edge cases
+//calculate() function that calculates input and stores in variable
+const calculate = () => {
+    screen.input.forEach(element => {
+       if (screen.input[i] === '+') {
+
+       }
+       
+       
+    });
+}
+
+//output() calls render then returns answer
