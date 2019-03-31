6d27e7cdd4439fb1cb85ce38bf4718a7bca0b43d
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-03-31 08:54:27 -0600

    clarified psuedo code. wrote and tested more functions

--- js/app.js
+++ js/app.js
@@ -1,21 +1,53 @@
-
+// operation should start as equals() - returns input converted to number
+//use jQuery to collect text from mouse clicks in input array
+//if number button - add to input array
+//if operator button - replace stored operator 
+//if equals button - perform object's stored operation on valueA and valueB
+    //return result to screen
+//if operator button store screen results to input
+//if clear button render all
 
 const calculon = {
-    input:[],
-    valueA: [],
+    input:["4","3","2"],
+    valueA: 0,
     valueB: [],
     operation: null
 }
 //functions
-const collectInput = (str) =>{
-
-};
+//temp function to test calculator, then will link to buttons
 //collects strings and stores in input
-//until an operator
-const storeInputs = (arr) =>{};
+const collectInput = (str) =>{
+calculon.input.push(str);
+console.log(calculon.input)
+};
+//if operator, store input in value A until another operator
+const checkForOperation = (str) =>{
+        if(str === "+"||"-"||"*"||"/"){
+            calculon.operation =str;
+            console.log(calculon.operation)
+    }
+}
+
 //concatinates strings and parses to number
 //stores in StoreA
-const storeValues = (num) =>{};
+const convertStrings =(arr) => {
+    arr = calculon.input;
+    for (let i = 0; i < calculon.input.length; i++) {
+        
+        calculon.valueA += arr[i];
+        calculon.valueA = parseInt(calculon.valueA);
+        console.log (calculon.valueA)
+    }
+}
+// const storeValues = (calculon.input) => {
+//     for (let i = 0; i < calculon.input.length; i++){
+//         calculon.valueA = [i]++
+//         parseInt(valueA);
+        
+//         console.log(calculon.valueA);
+//     }
+// }
+
 
 const newOperator = () =>{};
 //if input = operator, store in operator 
