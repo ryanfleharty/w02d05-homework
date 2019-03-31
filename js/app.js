4c26cd8ccede87f6647e5bac4168686be0378d5c
Destiny <dshillis1@gmail.com>
2019-03-31 16:45:05 -0600

    calculator works, doesn't show all the numbers

--- js/app.js
+++ js/app.js
@@ -41,18 +41,22 @@ let num2 = calculator.numTwo;
 let num2Array = calculator.num2Array;
 let num3Array = calculator.num3Array
 
+$('.outside').prepend('<div class="screen">0</div>');
+
 
 buttonClick = () => {
     $('button').unbind().click(function(e) {
     //grab text from button and put it in screen
     let btn = $(e.target).text();
 
-    $('.screen').text(btn); // make string
-    console.log(btn);
+    // make string
+    $('.screen').text(btn);
+
     
     if (btn != "=") {
     num1Array.push(btn);
     }
+
 
     num1Array.forEach((btn, index) => {
      
@@ -60,68 +64,52 @@ buttonClick = () => {
             num2Array = num1Array.slice(0, index);
             actionArray = num1Array.slice(index);
             num1Array.splice(0);
-
-            num1 = num2Array.join("");
-            console.log(num1);
-            num2 = num1Array.join("");
-            console.log(num2);
         }
     });
+
+    // put strings together into one string
+    // convert string to number
+
+    num1 = num2Array.join("");
+    num2 = num1Array.join("");
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    // console.log(num1);
+    // console.log(num2);
 
     if (btn = "=") {
         runActions();
     }
+
+    if (btn === "CLEAR"){
+        $('.screen').remove();
+    }
     });
 }
+
+
 
 $('button').on('click', buttonClick)
 
 
 
 
-// parseInt(num2Array);
-// parseInt(num1Array);
-
-
-// const doMath = () => {
-    
-//     num1Array.forEach((e, index) => {
-//         let btn = $(e.target).text();
-
-
-//         if (btn == isNaN(parseInt(e))){
-//             action = operation.splice(0, index);
-            
-//             if (e === "+"){
-//                 finalResult = parseInt(num1) + parseInt(num2);
-//             } else if (e === "-"){
-//                 finalResult = parseInt(num1) - parseInt(num2);
-//             } else if (e === "*"){
-//                 finalResult = parseInt(num1) * parseInt(num2);
-//             } else if (e === "/"){
-//                 finalResult = parseInt(num1) / parseInt(num2);
-//             }
-//         } else if (btn >= "0" && btn <= "9"){
-//             console.log(btn);
-//         }
-        
-//         $('.screen').text(btn);
-
-//     })
-// }
-
-
-
 runActions = () => {
 
-    if (actionArray === "+"){
-    total = num2Array + num1Array;
+    if (actionArray[0] === "+"){
+        total = num1 + num2;
+    } else if (actionArray[0] === "-"){
+        total = num1 - num2;
+    } else if (actionArray[0] === "x"){
+        total = num1 * num2;
+    } else if (actionArray[0] === "/"){
+        total = num1 / num2;
     }
     $('.screen').text(total);
     console.log(total);
 }
 
-// put strings together into one string
-// convert string to number
 
-
+const clear = () => {
+    $('.screen').remove();
+}
