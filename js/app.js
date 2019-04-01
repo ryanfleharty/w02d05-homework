5557bded5df5941ba452a4733de9865f7270af9d
WillyD <sopwill3@gmail.com>
2019-03-31 20:35:24 -0600

    completed calculator

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ console.log("Hello World!");
 
 
 
+{// PSUEDO CODE
 // When number is clicked it appears on screen
 // When multiple numbers clicked they create larger number
 
@@ -16,8 +17,16 @@ console.log("Hello World!");
 // complete operation
 // display answer on screen
 // save answer as new first value
+}
 
+let pendingCalculation = false;
 
+let fontSize = 54;
+
+const resetFontSize = () => {
+  fontSize = 54;
+  $('.screen').css('font-size', fontSize)
+}
 
 const add = (num1,num2) => {
   const answer = (num2 + num1);
@@ -67,40 +76,57 @@ const updateNumber = (button) => {
     } else if (button.target.id === "decimal") {
       screen.valueOne += '.';
     }
-    if (screen.currentValue === "one"){
+//    if (screen.currentValue === "one"){
       $('.screen').text(screen.valueOne)
-    } else {
-      $('.screen').text(screen.valueTwo)
-    }
-    console.log(screen.valueOne);
-    console.log(screen.valueTwo);
+//    } else {
+//      $('.screen').text(screen.valueTwo)
+//    }
+}
+
+const dynamicText = () => {
+if (screen.valueOne.length > 16 && screen.valueOne.length < 21) {
+  $('.screen').css('font-size', fontSize)
+  fontSize -= 2.7;
+} else if (screen.valueOne.length > 16 && screen.valueOne.length < 29) {
+  $('.screen').css('font-size', fontSize)
+  fontSize -= 1.5;
+} else if (screen.valueOne.length > 16 && screen.valueOne.length < 42 ) {
+  $('.screen').css('font-size', fontSize)
+  fontSize -= .75;
+}
 }
 
 
 
+// THIS OBJECT ACTS AS A STORAGE/DISPLY FOR CALCULATOR
 const screen = {
   display: null,
-  currentValue: "one",
+//  currentValue: "one",
   valueOne: "",
   valueTwo: "",
+  valueOld: "",
+  equalChain: false,
   valueOneNumber(){
     return Number(this.valueOne);
   },
   valueTwoNumber(){
     return Number(this.valueTwo)
   },
+  valueOldNumber(){
+    return Number(this.valueOld)
+  },
   operation: "",
-  calculate(){
+  calculate(one,two){
     if (this.operation === "add"){
-      add(this.valueOneNumber(),this.valueTwoNumber());
+      add(one,two);
     } else if (this.operation === "subtract"){
-      subtract(this.valueOneNumber(),this.valueTwoNumber());
+      subtract(one,two);
     } else if (this.operation === "multiply"){
-      multiply(this.valueOneNumber(),this.valueTwoNumber());
+      multiply(one,two);
     } else if (this.operation === "divide"){
-      divide(this.valueOneNumber(),this.valueTwoNumber());
+      divide(one,two);
     } else {
-      this.display = "ERROR--MISSING OPERATION";
+      this.display = "ERROR--NEED OPERATION";
     }
 //    this.operation = "";
     $('.screen').append(this.display);
@@ -108,37 +134,76 @@ const screen = {
 }
 
 
+
 $('#equals').on('click', function(){
+  resetFontSize();
+  pendingCalculation = false;
+  if (!screen.equalChain){
+    screen.valueOld = screen.valueOne;
+  }
   $('.screen').text("");
-  screen.calculate()
+  console.log(screen.equalChain);
+  console.log(`${screen.valueOld} -- valueOld`);
+  console.log(`${screen.valueOne} -- valueOne`);
+  console.log(`${screen.valueTwo} -- valueTwo`);
+  if (screen.equalChain){
+     screen.calculate(screen.valueOldNumber(),screen.valueOneNumber());
+  } else {
+    screen.calculate(screen.valueOneNumber(),screen.valueTwoNumber())
+  }
+  screen.equalChain = true;
+  console.log(screen.equalChain);
+  console.log(`${screen.valueOld} -- valueOld`);
+  console.log(`${screen.valueOne} -- valueOne`);
+  console.log(`${screen.valueTwo} -- valueTwo`);
 })
 
 $('#add').on('click', function(){
+if (pendingCalculation === false) {
   screen.operation = "add";
   screen.valueTwo = screen.valueOne;
   screen.valueOne = "";
-  screen.currentValue = "Two"
+  screen.equalChain = false;
+  pendingCalculation = true;
+  $('.screen').text("")
+  resetFontSize();
+}
 })
 
 $('#subtract').on('click', function(){
+  if (pendingCalculation === false) {
   screen.operation = "subtract";
   screen.valueTwo = screen.valueOne;
   screen.valueOne = "";
-  screen.currentValue = "Two"
+  screen.equalChain = false;
+  pendingCalculation = true;
+  $('.screen').text("")
+  resetFontSize();
+}
 })
 
 $('#multiply').on('click', function(){
+  if (pendingCalculation === false) {
   screen.operation = "multiply";
   screen.valueTwo = screen.valueOne;
   screen.valueOne = "";
-  screen.currentValue = "Two"
+  screen.equalChain = false;
+  pendingCalculation = true;
+  $('.screen').text("")
+  resetFontSize();
+}
 })
 
 $('#divide').on('click', function(){
+  if (pendingCalculation === false) {
   screen.operation = "divide";
   screen.valueTwo = screen.valueOne;
   screen.valueOne = "";
-  screen.currentValue = "Two"
+  screen.equalChain = false;
+  pendingCalculation = true;
+  $('.screen').text("")
+  resetFontSize();
+}
 })
 
 $('#clear').on('click', function(){
@@ -146,5 +211,10 @@ $('#clear').on('click', function(){
 })
 
 $('.number').on('click', function(e){
+if (screen.valueOne.length < 131) {
+  dynamicText()
   updateNumber(e);
+} else {
+  alert("Maximum Input Reached!")
+}
 })
