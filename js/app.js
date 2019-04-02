9e19914863db8fbe6b1537ebc7dbd320d23dcade
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-04-02 08:16:48 -0600

    adding jQ and rethinking js code. screen works

--- js/app.js
+++ js/app.js
@@ -1,121 +1,91 @@
 
 const calculon = {
-    input: [],
-    valueA: 0,
-    valueB: 0,
-    operation: 'e',
-    output: 0,
+    valueA: '',
+    valueB: '',
+    operation: '',
+    output: '',
 }
 
-const collectInput = (str) =>{
-    //if equals
-    //push a to b
-    //input to a
-    //run stored operation
-    //return to screen and push return to a
 
-if(str === 'e'){
-    calculon.valueB = calculon.valueA;
-        if (calculon.operation === "+") {
-            calculon.output = calculon.valueA + calculon.valueB
-            console.log(calculon.output);
-        } else if (calculon.operation === '-') {
-             calculon.output = calculon.valueA - calculon.valueB
-             console.log(calculon.output);
-        } else if (calculon.operation === '*') {
-            calculon.output = calculon.valueA * calculon.valueB
-            console.log(calculon.output);
-        } else {
-           calculon.output = calculon.valueA / calculon.valueB
-           console.log(calculon.output);
-        }
-        calculon.valueA = calculon.output
-        clear()
-    }else if (str === "+" || str === '*' || str === '-' || str === "/"){
-            for (let i = 0; i < calculon.input.length; i++) {
-                calculon.valueA += calculon.input[i];
-                calculon.valueA = parseInt(calculon.valueA);
-            }
-            calculon.operation = str
-            calculon.input = []
-            console.log(calculon)
-        } else {
-            if (calculon.valueA){
-                calculon.valueB = parseInt(str)
-                console.log(calculon)
-            }else{
-                calculon.input.push(str);
-                console.log(calculon)
-            }
-    }
+
+
+
+const collectInput = (btnValue) => {
+    console.log('input collected');
+    return btnValue
 }
 
-//if operator
-    //move input to a
-    //store operator value
-//if number
-    //push to input
-
-const clear = () => {
-//render()
-calculon.input = []
-calculon.valueA = 0
-calculon.valueB = 0
-calculon.operation = 'e'
-calculon.output= 0
-console.log('clear() has run')
-}
-
-//use jQuery to collect text from mouse clicks in input array
-//if number button - add to input array
-//button listeners - Numbers
 $("#1").click(function () {
     collectInput("1");
+    $('h3').text("1")
+    console.log('clicked 1')
 });
 $("#2").click(function () {
     collectInput("2");
+    $('h3').text("2")
+    console.log('clicked 2')
 });
 $("#3").click(function () {
     collectInput("3");
+    $('h3').text("3")
+    console.log('clicked 3')
 });
 $("#4").click(function () {
     collectInput("4");
+    $('h3').text("4")
+    console.log('clicked 4')
 });
 $("#5").click(function () {
     collectInput("5");
+    $('h3').text("5")
+    console.log('clicked 5')
 });
 $("#6").click(function () {
     collectInput("6");
+    $('h3').text("6")
+    console.log('clicked 6')
 });
 $("#7").click(function () {
     collectInput("7");
+    $('h3').text("7")
+    console.log('clicked 7')
 });
 $("#8").click(function () {
     collectInput("8");
+    $('h3').text("8")
+    console.log('clicked 8')
 });
 $("#9").click(function () {
     collectInput("9");
+    $('h3').text("9")
+    console.log('clicked 9')
 });
 $("#0").click(function () {
     collectInput("0");
+    $('h3').text("0")
+    console.log('clicked 0')
 });
 //button listeners - operators
 //if operator button - replace stored operator 
 //if operator button store screen results to input
 $("#add").click(function () {
     collectInput("+")
+    $('h3').text("+")
     console.log('+')
 });
 $("#sub").click(function () {
     collectInput("-")
+    $('h3').text("-")
     console.log('-')
 });
 $("#mult").click(function () {
     collectInput("*")
-    console.log('*')
+    $('h3').text("x")
+    console.log('x')
 });
 $("#div").click(function () {
     collectInput("/")
+    $('h3').text("/")
     console.log('/')
 });
 //button listeners - equals
@@ -123,16 +93,100 @@ $("#div").click(function () {
 //return result to screen
 $("#e").click(function () {
     collectInput('e')
+    $('h3').text("=")
     console.log('= clicked')
 });
-//button listeners - clear
+// //button listeners - clear
 
-//if clear button render all
+// //if clear button render all
 $("#c").click(function () {
-    clear();
+    $('h3').text("")
+    //clear();
     console.log('c clicked')
 });
 
-//use jQuery to show inputs and out puts on the screen
-//test
-//adjust UX 
+// //use jQuery to show inputs and out puts on the screen
+
+
+// //test
+// //adjust UX 
+
+
+
+
+
+
+
+
+
+
+
+// const checkNumbers = (str) => {
+// if ((str != '+') || 
+//     (str != '-') || 
+//     (str != '*') || 
+//     (str != '/') ||
+//     (str != 'e') && 
+//     (calculon.valueA === '')){
+//         calculon.valueA = str
+//         parseInt(calculon.valueA)
+//         displayInput(calculate.valueA)
+//     }else {
+//         calculon.valueB = str
+//         parseInt(calculon.valueB)
+//         displayInput(calculate.valueB)
+//     }
+// }
+// const checkOperator = (str) => {
+//     if ((str === "+") || (str === "-") || (str === "*") || (str === "/") && (calculon.operation === '')){
+//         calculon.operation = str
+//     }else {calculate()}
+// }   
+// const calculate = (operation) =>{
+//     if (calculon.operation === "+") {
+//         calculon.output = calculon.valueA + calculon.valueB
+//         console.log(calculon.output);
+//         displayOutput();
+//     } else if (calculon.operation === '-') {
+//         calculon.output = calculon.valueA - calculon.valueB
+//         console.log(calculon.output);
+//         displayOutput();
+//     } else if (calculon.operation === '*') {
+//         calculon.output = calculon.valueA * calculon.valueB
+//         console.log(calculon.output);
+//         displayOutput();
+//     } else {
+//         calculon.output = calculon.valueA / calculon.valueB
+//         console.log(calculon.output);
+//         displayOutput();
+//     }
+//     calculon.valueA = calculon.output
+//     clear()
+// }
+// const clear = () => {
+// //render()
+// calculon.input = []
+// calculon.valueA = 0
+// calculon.valueB = 0
+// calculon.operation = 'e'
+// calculon.output= 0
+// console.log('clear() has run')
+// }
+// const displayInput = ()=>{
+//     $('#screen').text(str)
+// }
+// const displayOutput = ()=>{
+//     $('#screen').text(`${calculon.output}`)
+//     calculon.output = calculate.valueA 
+// }
+// const collectInput = () =>{
+//     if (checkNumbers()){
+//         console.log(calculon)
+//     }else{checkOperator()
+//         console.log(calculon)
+//     }
+// }
+// //use jQuery to collect text from mouse clicks in input array
+// //if number button - add to input array
+// //button listeners - Numbers
+
